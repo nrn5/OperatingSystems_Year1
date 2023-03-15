55db1de3f6b68347689f3435a74abb70770939a1
nrn5 <124163358+nrn5@users.noreply.github.com>
2023-03-15 17:15:02 +0000

    codes

--- CA/selectmen.sh
+++ CA/selectmen.sh
@@ -1,7 +1,31 @@
-PS3="*Bash Commands*"
+PS3="Enter your choice:"
 
 select choice in List Show Show2 Display Backup Exit
 
 do
     echo "Selected: $choice"
+
+        case $choice in
+        "List")
+            stuff;;
+        
+        "Show")
+            stuff;;
+        
+        "Show2")
+            stuff;;
+
+        "Display")
+            stuff;;
+
+        "Backup")
+            stuff;;
+
+        "Exit")
+            echo "Exiting..."
+            break;;
+
+        esac
+
+
 done
